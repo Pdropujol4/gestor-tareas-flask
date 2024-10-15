570b4230be629be79e81ede25fc168c6d72c96e5
Pedro Pujol <pedropujol4@gmail.com>
2024-10-15 17:55:04 +0200

    Añadido CRUD de tareas y autenticación de usuarios

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Peedrito4' 
+login_manager = LoginManager(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Logged in successfully as {form.email.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('login.html', form=form)
 
 @app.route('/')
 def home():
@@ -9,4 +34,12 @@ def home():
 if __name__ == '__main__':
     app.run(debug=True)
     
-    
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Task('{self.title}', '{self.date_posted}')"
